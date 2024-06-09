b781d1029d34008a17492220c22560463e16bb2a
CyanFaux <santafox26@gmail.com>
2024-06-09 14:32:04 -0400

    "Josh's files"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,37 @@
-import "./App.css";
-import React from "react";
-import Header from "./components/Header";
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import SearchBar from './components/searchBar';
+import { createResource } from './Request';
 
 function App() {
+  let [searchTerm, setSearchTerm] = useState('')
+  let [data, setData] = useState(null)
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearchTerm(term)
+  }
+
+  useEffect(() => {
+    if(searchTerm) {
+      // setData(fetchData(searchTerm))
+      const fetchData = async () => {
+        const resource = createResource(searchTerm)
+        try{
+          const res = await resource.result.read()
+          setData(res)
+          console.log(res)
+        }catch(err){
+          console.error(err)
+        }
+      }
+      fetchData()
+    }
+  }, [searchTerm])
+
   return (
-    <div>
-      <Header />
-      <main>
-        <h1>MovieMind</h1>
-      </main>
+    <div className="App">
+    <SearchBar handleSearch={handleSearch} />
     </div>
   );
 }
-
-export default App;
---
+++ frontend/src/Request.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const API_KEY = process.env.API_KEY
+const testPORT = process.env.PORT
+console.log(testPORT)
+const API_URL = 'http://www.omdbapi.com/';
+const url = `${API_URL}?apikey=${API_KEY}&t=`;
+
+const fetchSearch = async (searchTerm) => {
+    const response = await fetch(url + searchTerm)
+    const resData = await response.json()
+    return resData.results
+}
+
+const wrapPromise = (promise) => {
+    let status = 'pending'
+    let result = ''
+
+    let suspender = promise.then(response => {
+        status = 'success'
+        result = response
+    }, err => {
+        status = 'error'
+        result = err
+    })
+
+    return {
+        read() {
+            if(status === 'pending') {
+                throw suspender
+            } else if(status === 'error') {
+                throw result
+            }
+
+            return result
+        }
+    }
+}
+
+export const createResource = (searchTerm) => {
+    return {
+        result: wrapPromise(fetchSearch(searchTerm))
+    }
+}
