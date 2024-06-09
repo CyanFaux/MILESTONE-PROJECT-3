9689c05b0d9b3ec11cb2f960515fb70880f9b26f
Josh Rivera <beps@Mactop.local>
2024-06-09 09:29:23 -0500

    Added fetch request with suspender

--- frontend/src/Request.js
+++ frontend/src/Request.js
@@ -1,7 +1,40 @@
-
 const API_KEY = process.env.API_KEY
 const API_URL = 'http://www.omdbapi.com/';
+const url = `${API_URL}?apikey=${API_KEY}&t=`;
 
+const fetchSearch = async (searchTerm) => {
+    const response = await fetch(API_URL + searchTerm)
+    const resData = await response.json()
+    return resData.results
+}
 
+const wrapPromise = (promise) => {
+    let status = 'pending'
+    let result = ''
 
-export default Request
+    let suspender = promise.then(response => {
+        status = 'success'
+        result = response
+    }, err => {
+        status = 'error'
+        result = err
+    })
+
+    return {
+        read() {
+            if(status === 'pending') {
+                throw suspender
+            } else if(status === 'error') {
+                throw result
+            }
+
+            return result
+        }
+    }
+}
+
+export const createResource = (searchTerm) => {
+    return {
+        result: wrapPromise(fetchSearch(searchTerm))
+    }
+}
