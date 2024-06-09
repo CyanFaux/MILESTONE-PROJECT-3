85c0297dd19b4c5d0166987c8b0ff81e0ee165d9
CyanFaux <santafox26@gmail.com>
2024-06-09 10:38:19 -0400

    "added backend files"

--- backend/models/review.js
+++ backend/models/review.js
@@ -1,7 +1,29 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    
-}
+  class Review extends Model {
+    static associate({ User, Movie }) {
+      Review.belongsTo(Movie, { as: "movie", foreignKey: "movie_id" });
+      Review.belongsTo(User, { as: "author", foreignKey: "author_id" });
+    }
+  }
+  Review.init(
+    {
+      reviewId: {
+        type: DataTypes.SMALLINT,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      movieId: DataTypes.SMALLINT,
+      authorId: DataTypes.SMALLINT,
+      content: DataTypes.STRING,
+      rating: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      underscored: true,
+      modelName: "Review",
+    }
+  );
+  return Review;
+};
