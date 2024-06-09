d5e9008a2161ea0c1e1e3702e4e8eff05177fe81
Josh Rivera <beps@Mactop.local>
2024-06-09 09:44:27 -0500

    Removed unnecessary code from Main.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import SearchBar from './components/searchBar';
 
 function App() {
+  let [searchTerm, setSearchTerm] = useState('')
+  
+  useEffect(() => {
+    if(searchTerm) {
+      setData(fetchData(searchTerm))
+    }
+  }, [searchTerm])
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearchTerm(term)
+  }
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar handleSearch={handleSearch} />
     </div>
   );
 }
