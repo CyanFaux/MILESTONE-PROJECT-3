9b6e40d1adfb59a7478ad9aed12118505ea3c2bc
CyanFaux <santafox26@gmail.com>
2024-06-11 19:58:56 -0400

    "merge with git nathan"

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
-const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const app = express();
 
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+// serve static front end in production mode
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+}
 
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/movies', require('./controllers/movies'))
+app.use('/users', require('./controllers/users'))
+
+// Listen for Connections
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)
 })
