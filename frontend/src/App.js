81c765c41b12a257ca894d2278b45ca8186dc7c4
Josh Rivera <beps@Mactop.local>
2024-06-13 17:14:00 -0500

    Moved functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
-import React, {useState, useEffect, Suspense} from 'react';
+import React from 'react';
 import './App.css';
-import SearchBar from './components/searchBar';
-import { createResource } from './Request';
-import Display from './components/Display';
-import Loading from './Loading';
+import Navigation from './components/Navigation';
 // import { render } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
     </div>
   );
 }
