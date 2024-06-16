f17b3ae61c33f8e13c838763a6b880b089957974
CyanFaux <santafox26@gmail.com>
2024-06-16 11:06:58 -0400

    NO MORE ERRORS OMGGG

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useContext } from "react";
 import { useNavigate } from "react-router";
-import UserContext from "../contexts/CurrentUser";
+import { UserContext } from "../contexts/CurrentUser";
 import SearchBar from "./searchBar";
 import { createResource } from "../Request";
 
