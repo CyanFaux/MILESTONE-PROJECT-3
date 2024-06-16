0f3b8c7f3a88f542742ab90943b642eaa0951c6b
eachoat <eachoat@gmail.com>
2024-06-16 12:54:38 -0400

    Movie Mind Logo

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,4 +1,4 @@
-import { useState, createContext, useContext } from "react";
+import { useState, useContext } from "react";
 import { useNavigate } from "react-router";
 import { UserContext } from "../contexts/CurrentUser";
 import SearchBar from "./searchBar";
