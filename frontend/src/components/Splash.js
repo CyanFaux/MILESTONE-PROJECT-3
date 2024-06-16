a72917bcc4ea647db45bed673b92c6014ac3a4f7
Josh Rivera <beps@Mactop.local>
2024-06-16 13:38:53 -0500

    Added image to splash page

--- frontend/src/components/Splash.js
+++ frontend/src/components/Splash.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Footer from "./Footer";
+import Image from 'react-bootstrap/Image';
 
 function Splash() {
     return (
         <React.Fragment>
             <h1>Welcome to Movie Mind</h1>
-            <img src="" alt="Movie Mind Logo Image" />
+            <Image src="../../public/images/photo-1560109947-543149eceb16.avif" alt="Movie Mind Logo Image, provided by Anika De Klerk on Unsplash.com" rounded/>
             <Footer />
         </React.Fragment>
     )
